283f30cbd016022fb3f31ef2c6a926d3e6401544
u1979272 <u1979272@campus.udg.edu>
2024-05-16 00:53:11 +0200

    Cosmetical editing of the first level

---
+++ tiled/Gear_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Gear_tile" tilewidth="37" tileheight="44" tilecount="4" columns="2">
+ <image source="../resources/props/gear_tile.png" trans="ff00ff" width="74" height="88"/>
+</tileset>
