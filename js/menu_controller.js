2efcc421772855c3fa30bbc304fd38d411687389
Natalya Golembyovska <125288211+u1979272@users.noreply.github.com>
2024-05-21 16:31:15 +0200

    Update menu_controller.js

--- js/menu_controller.js
+++ js/menu_controller.js
@@ -1,13 +1,13 @@
 function platform_game(){
 	localStorage.setItem("carregar",0);
-	loadpage("./html/platform.html");
+	loadpage("https://lucem-joc-en-grup.github.io/Per-Aspera-Ad-Lucem/html/platform.html");
 
 }
 
 function load(){
 	// loadpage("./html/load.html");
 	localStorage.setItem("carregar",1);
-	loadpage("./html/platform.html");
+	loadpage("https://lucem-joc-en-grup.github.io/Per-Aspera-Ad-Lucem/html/platform.html");
 	
 }
 
